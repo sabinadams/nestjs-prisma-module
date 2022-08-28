b3d744dade007b5df58abc71acdf0a2baab4bd52
Sabin Adams <sabintheworld@gmail.com>
2022-08-28 01:26:42 -0700

    Fixes factory tests

--- test/connection.factory.test.ts
+++ test/connection.factory.test.ts
@@ -1,14 +1,31 @@
-import { vi, describe, expect, it } from 'vitest';
+import { vi, describe, expect, it, beforeEach } from 'vitest';
 import factory from '../src/connection.factory';
 import PrismaService from '../src/prisma.service';
 import { REQUEST } from '@nestjs/core';
 import { BadRequestException, Scope } from '@nestjs/common';
-import { Request } from 'node-fetch';
-
-const service = new PrismaService(null, 'file:./dev.db', { test: 1 }, 'test');
-const provider = factory('TEST', service);
+import PrismaMock from './helpers/PrismaMock';
 
 describe('connection.factory.ts', () => {
+  let provider;
+  const fakeRequest = {
+    headers: {
+      'x-tenant-id': 'test',
+      get() {
+        return this;
+      },
+    },
+  };
+  const service = new PrismaService(
+    PrismaMock,
+    'file:./dev.db',
+    { test: 1 },
+    'test',
+    true,
+  );
+  beforeEach(() => {
+    provider = factory('TEST', true, service);
+  });
+
   it('Should return a Provider', () => {
     expect(provider.provide).toBe('TEST');
     expect(provider.scope).toBe(Scope.REQUEST);
@@ -16,32 +33,91 @@ describe('connection.factory.ts', () => {
     expect(provider).toHaveProperty('useFactory');
   });
 
-  it("Provider's `useFactory` should return a connection when valid tenant sent", async () => {
-    // Mock the request
-    const request = new Request('http://www.google.com');
-    // Add the required header
-    request.headers.append('x-tenant-id', 'test');
-    // Mock the service response to avoid needing a connection
-    const spy = vi.spyOn(service, 'getConnection');
-    spy.mockImplementationOnce(() => 'test-connection');
-    // Run the function
-    const connection = await provider.useFactory(request);
+  describe('Multitenancy', () => {
+    it("Provider's `useFactory` should return a connection when valid tenant sent", async () => {
+      // Mock the service response to avoid needing a connection
+      const spy = vi.spyOn(service, 'getConnection');
+      spy.mockImplementationOnce((tenant) => tenant);
+      // Run the function
+      const connection = await provider.useFactory(fakeRequest);
+      expect(service.getConnection).toHaveBeenCalledWith('test');
+      expect(connection).toBe('test');
+    });
 
-    expect(connection).toBe('test-connection');
+    it("Provider's `useFactory` should return a BadRequestException when no tenant sent", async () => {
+      // Run the function
+      await expect(
+        provider.useFactory({
+          headers: {
+            get(name) {
+              return this[name];
+            },
+          },
+        }),
+      ).rejects.toThrowError('⛔️ Invalid Request Options - Tenant');
+
+      // Run the function
+      await provider
+        .useFactory({
+          headers: {
+            get(name) {
+              return this[name];
+            },
+          },
+        })
+        .catch((e) => {
+          expect(e).toBeInstanceOf(BadRequestException);
+        });
+    });
   });
-
-  it("Provider's `useFactory` should return a BadRequestException when no", async () => {
-    // Mock the request
-    const request = new Request('http://www.google.com');
-
-    // Run the function
-    await expect(provider.useFactory(request)).rejects.toThrowError(
-      '⛔️ Invalid Request Options - Tenant',
+  describe('Single Tenant', () => {
+    let provider;
+    const fakeRequest = {
+      headers: {
+        get(name) {
+          return this[name];
+        },
+      },
+    };
+    const service = new PrismaService(
+      PrismaMock,
+      undefined,
+      { test: 1 },
+      'test',
     );
-
-    // Run the function
-    await provider.useFactory(request).catch((e) => {
-      expect(e).toBeInstanceOf(BadRequestException);
+    beforeEach(() => {
+      provider = factory('TEST', false, service);
+    });
+    it('Should return a Provider', () => {
+      expect(provider.provide).toBe('TEST');
+      expect(provider.scope).toBe(Scope.REQUEST);
+      expect(provider.inject[0]).toBe(REQUEST);
+      expect(provider).toHaveProperty('useFactory');
+    });
+    it("Provider's `useFactory` should return a connection even with no tenant header", async () => {
+      // Mock the service response to avoid needing a connection
+      const spy = vi.spyOn(service, 'getConnection');
+      spy.mockImplementationOnce((tenant) => tenant);
+      // Run the function
+      const connection = await provider.useFactory(fakeRequest);
+      expect(service.getConnection).toHaveBeenCalledWith('DEFAULT');
+      expect(connection).toBe('DEFAULT');
+    });
+    it("Provider's `useFactory` should return a connection named DEFAULT even tenant header sent", async () => {
+      // Mock the service response to avoid needing a connection
+      const spy = vi.spyOn(service, 'getConnection');
+      spy.mockImplementationOnce((tenant) => tenant);
+      // Run the function
+      const connection = await provider.useFactory({
+        headers: {
+          'x-tenant-id': 'test-tenant',
+          get(name) {
+            return this[name];
+          },
+        },
+      });
+      expect(service.getConnection).toHaveBeenCalledWith('DEFAULT');
+      expect(connection).toBe('DEFAULT');
     });
   });
 });
