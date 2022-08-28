29b21c1388f6c95b39b044d37edb84130d548510
Sabin Adams <sabintheworld@gmail.com>
2022-08-28 00:40:40 -0700

    Fixes module integration tests

--- test/prisma.module.integration.test.ts
+++ test/prisma.module.integration.test.ts
@@ -6,89 +6,178 @@ import { ContextIdFactory } from '@nestjs/core';
 import PrismaMock from './helpers/PrismaMock';
 
 describe('Integration Tests', () => {
-  describe('With Initializer', () => {
-    let userController: UserController;
-    const $initializerTest = vi.fn((tenant: string) => tenant);
-    beforeEach(async () => {
-      const moduleRef = await Test.createTestingModule({
-        controllers: [UserController],
-        imports: [
-          PrismaModule.register({
-            client: {
-              class: PrismaMock,
-              initializer: (client, tenant) => {
-                $initializerTest(tenant);
-                return client;
+  describe('Multitenancy Active', () => {
+    describe('With Initializer', () => {
+      let userController: UserController;
+      const $initializerTest = vi.fn((tenant: string) => tenant);
+      beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+          controllers: [UserController],
+          imports: [
+            PrismaModule.register({
+              client: {
+                class: PrismaMock,
+                initializer: (client, tenant) => {
+                  $initializerTest(tenant);
+                  return client;
+                },
+              },
+              multitenancy: true,
+              datasource: 'file:./dev.db',
+              name: 'USERS',
+            }),
+          ],
+        }).compile();
+
+        const contextId = ContextIdFactory.create();
+        moduleRef.registerRequestByContextId(
+          {
+            headers: {
+              'x-tenant-id': 'test-tenant',
+              get() {
+                return this;
               },
             },
-            name: 'USERS',
-          }),
-        ],
-      }).compile();
+          },
+          contextId,
+        );
+        userController = await moduleRef.resolve<UserController>(
+          UserController,
+          contextId,
+        );
+      });
 
-      const contextId = ContextIdFactory.create();
-      moduleRef.registerRequestByContextId(
-        {
-          headers: {
-            'x-tenant-id': 'test-tenant',
-            get() {
-              return this;
+      it('Should register a usable Prisma service', async () => {
+        await userController.getUserCount();
+        expect(userController.users).toBeInstanceOf(PrismaMock);
+        expect(userController.users.user.count).toBeCalled();
+        expect(userController.users.user.count).toHaveReturnedWith(1);
+        expect($initializerTest).toHaveBeenCalledWith('test-tenant');
+      });
+    });
+    describe('Class Only', () => {
+      let userController: UserController;
+      beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+          controllers: [UserController],
+          imports: [
+            PrismaModule.register({
+              client: PrismaMock,
+              multitenancy: true,
+              datasource: 'file:./dev.db',
+              name: 'USERS',
+            }),
+          ],
+        }).compile();
+
+        const contextId = ContextIdFactory.create();
+        moduleRef.registerRequestByContextId(
+          {
+            headers: {
+              'x-tenant-id': 'test-tenant',
+              get() {
+                return this;
+              },
             },
           },
-        },
-        contextId,
-      );
-      userController = await moduleRef.resolve<UserController>(
-        UserController,
-        contextId,
-      );
-    });
+          contextId,
+        );
+        userController = await moduleRef.resolve<UserController>(
+          UserController,
+          contextId,
+        );
+      });
 
-    it('Should register a usable Prisma service', async () => {
-      await userController.getUserCount();
-      expect(userController.users).toBeInstanceOf(PrismaMock);
-      expect(userController.users.user.count).toBeCalled();
-      expect(userController.users.user.count).toHaveReturnedWith(1);
-      expect($initializerTest).toHaveBeenCalledWith('test-tenant');
+      it('Should register a usable Prisma service', async () => {
+        await userController.getUserCount();
+        expect(userController.users).toBeInstanceOf(PrismaMock);
+        expect(userController.users.user.count).toBeCalled();
+        expect(userController.users.user.count).toHaveReturnedWith(1);
+      });
     });
   });
-  describe('Class Only', () => {
-    let userController: UserController;
-    beforeEach(async () => {
-      const moduleRef = await Test.createTestingModule({
-        controllers: [UserController],
-        imports: [
-          PrismaModule.register({
-            client: PrismaMock,
-            datasource: 'file:./dev.db',
-            name: 'USERS',
-          }),
-        ],
-      }).compile();
+  describe('No Multitenancy', () => {
+    describe('With Initializer', () => {
+      let userController: UserController;
+      const $initializerTest = vi.fn((tenant: string) => tenant);
+      beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+          controllers: [UserController],
+          imports: [
+            PrismaModule.register({
+              client: {
+                class: PrismaMock,
+                initializer: (client, tenant) => {
+                  $initializerTest(tenant);
+                  return client;
+                },
+              },
+              name: 'USERS',
+            }),
+          ],
+        }).compile();
 
-      const contextId = ContextIdFactory.create();
-      moduleRef.registerRequestByContextId(
-        {
-          headers: {
-            'x-tenant-id': 'test-tenant',
-            get() {
-              return this;
+        const contextId = ContextIdFactory.create();
+        moduleRef.registerRequestByContextId(
+          {
+            headers: {
+              get() {
+                return this;
+              },
             },
           },
-        },
-        contextId,
-      );
-      userController = await moduleRef.resolve<UserController>(
-        UserController,
-        contextId,
-      );
-    });
+          contextId,
+        );
+        userController = await moduleRef.resolve<UserController>(
+          UserController,
+          contextId,
+        );
+      });
 
-    it('Should register a usable Prisma service', async () => {
-      await userController.getUserCount();
-      expect(userController.users).toBeInstanceOf(PrismaMock);
-      expect(userController.users.user.count).toBeCalled();
-      expect(userController.users.user.count).toHaveReturnedWith(1);
+      it('Should register a usable Prisma service', async () => {
+        await userController.getUserCount();
+        expect(userController.users).toBeInstanceOf(PrismaMock);
+        expect(userController.users.user.count).toBeCalled();
+        expect(userController.users.user.count).toHaveReturnedWith(1);
+        expect($initializerTest).toHaveBeenCalledWith('DEFAULT');
+      });
+    });
+    describe('Class Only', () => {
+      let userController: UserController;
+      beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+          controllers: [UserController],
+          imports: [
+            PrismaModule.register({
+              client: PrismaMock,
+              name: 'USERS',
+            }),
+          ],
+        }).compile();
+
+        const contextId = ContextIdFactory.create();
+        moduleRef.registerRequestByContextId(
+          {
+            headers: {
+              get() {
+                return this;
+              },
+            },
+          },
+          contextId,
+        );
+        userController = await moduleRef.resolve<UserController>(
+          UserController,
+          contextId,
+        );
+      });
+
+      it('Should register a usable Prisma service', async () => {
+        await userController.getUserCount();
+        expect(userController.users).toBeInstanceOf(PrismaMock);
+        expect(userController.users.user.count).toBeCalled();
+        expect(userController.users.user.count).toHaveReturnedWith(1);
+      });
     });
   });
 });
